25348f7cf8aac946cd1f28920f83bb2c60dcc49d
Anirudh S <107941714+AnirudhS1704@users.noreply.github.com>
2023-03-13 16:52:27 +0530

    Added client file
    Added home page and its react components

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HomeBody from './HeaderBody';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="">
+      <Header />
+      <HomeBody />
+    </div>
+  );
+}
+
+export default App;
