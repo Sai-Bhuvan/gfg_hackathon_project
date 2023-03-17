6b17617870508cee568e6d4f0fccc77bd20b127a
Anirudh S <107941714+AnirudhS1704@users.noreply.github.com>
2023-03-17 09:12:37 +0530

    updated header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,18 +1,27 @@
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
+  const [profileButton, setProfileButton] = useState("Login")
+  
+  useEffect(()=>{
+    console.log(window.localStorage.getItem('name'));
+    if (window.localStorage.getItem('name')) {
+      setProfileButton(window.localStorage.getItem('name'));
+    }
+}, []);
 
     return (
         <>
         <header>
-        <nav className="bg-white border-orange-200 px-2 sm:px-4 py-3 dark:bg-orange-300">
+        <nav className=" border-orange-200 px-2 sm:px-4 py-3 bg-appOrange bg-opacity-80">
     <div className="container flex flex-wrap items-center justify-between mx-auto">
-    <a href="/" className="flex items-center">
+    <a href="#" className="flex items-center">
         <img src={require('./images/logo.png')} className="h-6 mr-3 sm:h-9" alt="budgetx Logo" />
         <p className="self-center text-3xl whitespace-nowrap dark:text-black font-extrabold font-serif" >BudgetX</p>
     </a>
     <div className="flex items-center lg:order-2">
-        <Link to ="/signupPage" style={{marginRight:'10px'}}>Login/Sign&nbsp;Up</Link>
+        <Link to={"/login"} style={{marginRight:'10px'}}>{profileButton}</Link>
         <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
           <span className="sr-only">Open user menu</span>
           <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 ">
@@ -25,18 +34,18 @@ export default function Header() {
             <span className="block text-sm text-white dark:text-white">User</span>
             <span className="block text-sm font-medium text-white truncate dark:text-white">id@email.com</span>
           </div>
-          <ul className="py-2" aria-labelledby="user-menu-button">
+          <ul className="py-2 " aria-labelledby="user-menu-button">
             <li>
-              <a href="/" className="block px-4 py-2 text-sm text-gray-700 hover:bg-orange-100 dark:hover:bg-orange-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+              <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-orange-100 dark:hover:bg-orange-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
             </li>
             <li>
-              <a href="/" className="block px-4 py-2 text-sm text-gray-700 hover:bg-orange-100 dark:hover:bg-orange-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+              <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-orange-100 dark:hover:bg-orange-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
             </li>
             <li>
-              <a href="/" className="block px-4 py-2 text-sm text-gray-700 hover:bg-orange-100 dark:hover:bg-orange-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+              <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-orange-100 dark:hover:bg-orange-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
             </li>
             <li>
-              <a href="/" className="block px-4 py-2 text-sm text-gray-700 hover:bg-orange-100 dark:hover:bg-orange-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+              <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-orange-100 dark:hover:bg-orange-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
             </li>
           </ul>
         </div>
@@ -46,23 +55,23 @@ export default function Header() {
       </button>
     </div>
     <div className="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-      <ul className="flex flex-col p-4 mt-4 border border-orange-100 rounded-lg bg-orange-50 lg:flex-row lg:space-x-8 lg:mt-0 lg:text-sm lg:font-medium lg:border-0 lg:bg-white dark:bg-orange-100 lg:dark:bg-orange-200 dark:border-orange-100">
-        <li>
-          <a href="/" className="block py-2 pl-3 pr-4 text-red-400 bg-blue-700 rounded lg:bg-transparent lg:text-white lg:p-0 dark:text-white" style={{fontSize: '1.1rem'}} aria-current="page">Home</a>
-        </li>
-        <li>
-          <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-orange-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-orange-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-orange-700" style={{fontSize: '1.1rem'}}>About</a>
-        </li>
-        <li>
-          <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-orange-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-orange-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-orange-700" style={{fontSize: '1.1rem'}}>Services</a>
-        </li>
-        <li>
-          <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-orange-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-orange-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-orange-700" style={{fontSize: '1.1rem'}}>Pricing</a>
-        </li>
-        <li>
-          <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-orange-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-orange-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-orange-700" style={{fontSize: '1.1rem'}}>Contact</a>
-        </li>
-      </ul>
+      <div className="flex p-4 mt-1 border rounded-lg flex-col bg-appYellow bg-opacity-20 gap-4  lg:flex-row">
+        <p>
+          <a href="#" className="block py-2 pl-3 pr-4  text-gray-700 rounded hover:text-white bg-blue-700 lg:bg-transparent  lg:hover:text-blue-700 lg:text-white lg:p-0 dark:text-white" style={{fontSize: '1.1rem'}} aria-current="page">Home</a>
+        </p>
+        <p>
+          <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-white lg:p-0 dark:text-gray-400  lg:hover:text-blue-700 hover:bg-orange-100 lg:dark:hover:text-white dark:hover:bg-orange-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-orange-700" style={{fontSize: '1.1rem'}}>About</a>
+        </p>
+        <p>
+          <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-white hover:bg-orange-100 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-orange-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-orange-700" style={{fontSize: '1.1rem'}}>Services</a>
+        </p>
+        <p>
+          <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-white hover:bg-orange-100 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-orange-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-orange-700" style={{fontSize: '1.1rem'}}>Pricing</a>
+        </p>
+        <p>
+          <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-white hover:bg-orange-100 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-orange-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-orange-700" style={{fontSize: '1.1rem'}}>Contact</a>
+        </p>
+      </div>
     </div>
     </div>
   </nav>
