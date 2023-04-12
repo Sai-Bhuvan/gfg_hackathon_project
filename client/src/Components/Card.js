6baef8cbf57f9121dcae5d854859dd517f381497
Anirudh S <107941714+AnirudhS1704@users.noreply.github.com>
2023-04-12 14:26:45 +0530

    deleted client and api

--- client/src/Components/Card.js
+++
@@ -1,7 +0,0 @@
-export default function Card({children, padding}) {
-    return (
-        <div className={"bg-white rounded-2xl shadow-md shadow-gray-200 h-5/6 "+padding}>
-            {children}
-        </div>
-    )
-}
--- client/src/ExpensePage/ExpenseList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ExpenseList() {
-  return (
-    <div className='m-5'>
-      <h2 className='text-2xl'>Expenditure List</h2>
-    </div>
-  )
-}
