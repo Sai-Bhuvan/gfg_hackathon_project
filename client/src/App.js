936c6245c4d08043f98294bcbfd218bfcbfc9765
Anirudh S <107941714+AnirudhS1704@users.noreply.github.com>
2023-04-15 12:21:57 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import {Routes, Route}  from 'react-router-dom'
 import SignUpPage from "./Components/SignUpPage"
 import LoginPage from "./Components/LoginPage"
-import DashboardPage, { BarGraph, PieGraph } from './Components/AnalyseExpense';
+import DashboardPage from './Components/AnalyseExpense';
 import HomePage from './Components/HomePage';
 import LoginLanding from './Components/LoginLanding';
 import AddExpense from './Components/AddExpense';
 import ExpenseList from './Components/ExpenseList';
 import AddIncome from './Components/AddIncome';
 import InvestPage from './Components/InvestPage';
+import AboutPage from './Components/AboutPage';
 
 function App() {
   const id = window.localStorage.getItem('id');
@@ -24,6 +25,7 @@ function App() {
       <Route path={'/add/income/:id'} element = {<AddIncome />}/>
       <Route path={'/expenses/:id'} element = {<ExpenseList />}/>
       <Route path={'/investometer'} element = {<InvestPage/>}/>
+      <Route path={'/about'} element = {<AboutPage/>}/>
     </Routes>  
     </>
     
