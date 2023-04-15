1bc515f17b739d94d0d84ae08cad7835e768a063
Anirudh S <107941714+AnirudhS1704@users.noreply.github.com>
2023-04-15 12:16:54 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,19 @@ import './App.css';
 import {Routes, Route}  from 'react-router-dom'
 import SignUpPage from "./Components/SignUpPage"
 import LoginPage from "./Components/LoginPage"
-// import axios from 'axios';
 import DashboardPage, { BarGraph, PieGraph } from './Components/AnalyseExpense';
 import HomePage from './Components/HomePage';
 import LoginLanding from './Components/LoginLanding';
 import AddExpense from './Components/AddExpense';
 import ExpenseList from './Components/ExpenseList';
 import AddIncome from './Components/AddIncome';
-import FintechSolutions from './Components/FintechSolution';
-import EmiCalculater from './Components/EmiCalculater';
-import CompoundIntrestCal from './Components/CompoundIntrestCal';
-import SimpleIntrestCal from './Components/SimpleInterestCal';
-
-// axios.defaults.baseURL = "http://localhost:4000";
+import InvestPage from './Components/InvestPage';
 
 function App() {
   const id = window.localStorage.getItem('id');
   return (
     <>
-    {/* <Routes>
+    <Routes>
       <Route path = '/' element = { <HomePage/>}/> 
       <Route path='/signup' element = {<SignUpPage/>}/>
       <Route path='/login' element = {<LoginPage/>}/>
@@ -29,19 +23,8 @@ function App() {
       <Route path={'/add/expense/:id'} element = {<AddExpense />}/>
       <Route path={'/add/income/:id'} element = {<AddIncome />}/>
       <Route path={'/expenses/:id'} element = {<ExpenseList />}/>
-      <Route path={'/fintech-solutions'} element = {<FintechSolutions />}/>
-    </Routes> */}
-    {/* <EmiCalculater/> */}
-    {/* <CompoundIntrestCal/> */}
-    <SimpleIntrestCal/>
-    {/* <SignupPage/> */}
-     
-    {/* <LoginPage/> */}
-      {/* <div className="">
-             
-        
-      </div>   */}
-      
+      <Route path={'/investometer'} element = {<InvestPage/>}/>
+    </Routes>  
     </>
     
   );
